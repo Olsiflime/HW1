334fcc0f15dae0a41ccbcfe415a193d4aab57ca0
olsiflime <olsiflime@mail.ru>
2023-09-13 20:59:57 +0700

    HW2

---
+++ HW2/script.js
@@ -0,0 +1,53 @@
+let a = 20;
+alert(a);
+let b = 2007;
+alert(b);
+let c = 10;
+let d = 2;
+let summ = c + d;
+let diff = c - d;
+let mul = c * d;
+let div = c / d;
+alert(summ);
+alert(diff);
+alert(mul);
+alert(div);
+let e = 2;
+let result = e ** 5;
+alert(result);
+let f = 9;
+let g = 2;
+alert(f % g);
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+const user = {  name:`lexy`, age: 25, isAdmin: true};
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin); 
+
+user[`sity of residence`]=true;
+alert([`sity of residence`]);
+
+user.age=30;
+alert(user.age);
+
+delete user[`sity of residence`];
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+
+let nam = String(prompt('Твое имя?'));
+alert (`Привет, ${nam}!`);
+
+
