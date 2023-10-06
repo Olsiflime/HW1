97cde0b83223d482b8ecdff77076875389cf32e9
OLs <olsiflime@mail.ru>
2023-10-06 12:30:03 +0700

    hw7

--- HW7/script.js
+++ HW7/script.js
@@ -21,8 +21,28 @@ if (monthNumber === 1 || monthNumber === 2 || monthNumber === 12) {
   }
  }
 }
+//Игра 2
+
+let opcion = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+opcion = opcion.sort(() => Math.random() - 0.5)
+alert (`${opcion}`)
+let first = prompt ('Какая вкусняшка была первой?');
+let last = prompt ('Какая вкусняшка была последней?');
+
+let masFirst = (opcion[0].toLowerCase());
+
+console.log(masFirst)
+if (masFirst == first.toLowerCase) {
+  alert('yes')
+} else {
+  alert('no')
+  
+}
+// 
+
 
 //Домашка 7
+
 //Задание 1
 let names = 'Валера';
 names = names.toUpperCase(names);
@@ -30,30 +50,83 @@ console.log (names)
 
 //Задание 2
 const searchStart = ['Кошка', 'Кит', 'Комар', 'Носорог'];
+const ko = 'ко';
 
-console.log(searchStart.startsWith('Ко'))
+searchStart.forEach((searchStart) => {
+  if (searchStart.toLowerCase().startsWith(ko.toLowerCase())){
+    console.log(searchStart)
+  }
+})
 
 //Задание 3
+let a = [32.58884];
+
+console.log(Math.floor(a) ) //меньшее целое
+console.log(Math.ceil(a) ) //большее целое
+console.log(Math.round(a) ) //ближайшее целое
+
 
 //Задание 4
+const numb = [52, 53, 49, 77, 21, 32];
+ let max = Math.max(...numb)
+ let min = Math.min(...numb)
+
+ console.log(max)
+ console.log(min)
 
 //Задание 5
+function ranOm (nRan, xRan){
+  return Math.random( )* 10;
+}
+console.log(Math.round(ranOm()));
 
 //Задание 6
+// Не идет что-то
+const numRan = 5;
+function bred (p){
+  let six = [];
+  for (let i = 0; i< (six.lenght / 2); i++){
+    six[i] = i == numRan
+  return Math.floor(Math.random() * (p));
+}
+}
+let pol = bred (numRan);
+console.log(pol);
 
 //Задание 7
+function seven (r, t){
+  return Math.floor(Math.random() * (t-r)) + r;
+}
+let rez = seven(2, 5);
+console.log(rez)
 
 //Задание 8
-
+let myDate = new Date();
+console.log(myDate);
 
 //Задание 9
+
+let currentDate = new Date();
+currentDate.setDate (currentDate.getDate() + 73);
+
+console.log(currentDate)
 
 
 //Задание 10
 
-//Задание 11
+let dateNew = new Date();
+let monthRU = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+let dayRU = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье',];
 
-// Задание 12
+function rezult (dateNew){
+  let date = dateNew.getDate();
+ let month = dateNew.getMonth();
+ let year = dateNew.getFullYear();
+ let day = dateNew.getDay();
+ let time = dateNew.toTimeString();
+ console.log(`Сегодня ${date} ${monthRU[month]} ${year} года, ${dayRU[day-1]} ${time}`)
 
-//Задание 13
+}
+rezult(dateNew)
+
 
